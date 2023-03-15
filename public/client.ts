ddaf12ecfbdd2483190341998a8b580cf572b2d5
tn3rb <brent@eventespresso.com>
2023-03-15 13:33:45 -0700

    use sanity token

--- public/client.ts
+++ public/client.ts
@@ -5,5 +5,5 @@ export const client = createClient({
 	dataset: 'production', // or the name you chose in step 1
 	useCdn: true, // `false` if you want to ensure fresh data
 	apiVersion: '2023-01-01', // use current date (YYYY-MM-DD) to target the latest API version
-	// token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+	token: process.env.SANITY_TOKEN, // Only if you want to update content with the client
 });
