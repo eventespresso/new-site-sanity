c8ece893d2400d265c9911cde2784255aec5153e
tn3rb <brent@eventespresso.com>
2023-03-15 13:03:16 -0700

    add getStaticPaths()

--- public/src/pages/_app.tsx
+++ public/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import client from '../../client'
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    `*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug: string) => ({ params: { slug } })),
+    fallback: true,
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
