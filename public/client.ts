836a82bdf10b9cf9404705e66843cf296a272bb3
tn3rb <brent@eventespresso.com>
2023-03-15 13:21:24 -0700

    fix sanity client

--- public/client.ts
+++ public/client.ts
@@ -1,7 +1,9 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 
-export default sanityClient({
+export const client = createClient({
 	projectId: 'rojtizi9', // you can find this in sanity.json
 	dataset: 'production', // or the name you chose in step 1
 	useCdn: true, // `false` if you want to ensure fresh data
+	apiVersion: '2023-01-01', // use current date (YYYY-MM-DD) to target the latest API version
+	// token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 });
