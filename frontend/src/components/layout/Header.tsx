55e9e30af19422b7cabe96e281e7aebfeaa04460
tn3rb <brent@eventespresso.com>
2023-03-17 14:36:58 -0700

    use Image comp add pricing Link

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Header.module.scss'
 import layout from '@/styles/layout.module.scss'
@@ -8,10 +9,20 @@ const Header: React.FC = () => {
 			<div className={`${styles.header__wrap} ${layout.container}`}>
 				<Link href="/" className={styles.espresso_logo__link}>
 					<div className={styles.espresso_logo__img_wrap}>
-						<img src="/espresso-cup-logo.png" className={styles.espresso_logo__img} alt="Event Espresso" />
+						<Image src="/espresso-cup-logo.png"
+							className={styles.espresso_logo__img}
+							alt="Event Espresso"
+							height='48'
+							width='48'
+						/>
 					</div>
 					<span className={styles.espresso_logo__wordmark}>event espresso</span>
 				</Link>
+				<div className={styles.primary_nav}>
+					<Link href="/pricing" className={styles.primary_nav_link}>
+						pricing
+					</Link>
+				</div>
 			</div>
 		</header>
 	);
