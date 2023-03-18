04591fcd8134e0665eb47afb030100a0cfd1a007
tn3rb <brent@eventespresso.com>
2023-03-18 10:43:20 -0700

    add some more homepage content

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,16 @@
 import groq from 'groq'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { client } from '../../client'
+import eventEspressoAdmin from '../../public/event_espresso_admin_screenshot.png'
 
 import type { GetStaticProps } from 'next';
 import type { ContentProps } from '../../types';
 
-import home from '../styles/Homepage.module.scss'
-import layout from '@/styles/layout.module.scss'
+import buttons from '@/styles/Buttons.module.scss'
+import home from '@/styles/Homepage.module.scss'
+import layout from '@/styles/Layout.module.scss'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   // console.log('%c context', 'color: HotPink;', context);
@@ -26,9 +30,27 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const Home: React.FC<ContentProps> = ({ content }) => {
   console.log('%c homepage', 'color: HotPink;', content);
   return (
-    <section className={home.hero}>
-      <div className={layout.container}>
-        <h1>title: {content?.title}</h1>
+    <section className={`${layout.container} hero_section`}>
+      <div className={home.hero}>
+        <h1>
+          <span className={home.the_best}>the best</span>
+          <span className={home.event_reg}>Event Registration</span>
+          <span className={home.ticketing}>& Ticketing Plugin</span>
+          <span>for <span className={home.wordpress}>WordPress</span></span>
+        </h1>
+        <div className={home.hero_text}>
+          <p>Event Espresso is the best WordPress plugin for online event registration and ticketing - with the best full-time support.</p>
+          <p>Easily transform your existing WordPress website into a full-featured event registration and ticketing website.</p>
+        </div>
+        <Image src={eventEspressoAdmin} alt='screenshot of event espresso admin page' />
+        <div className={home.call_to_actions}>
+          <Link href="/buy-now" className={`${buttons.btn} ${buttons.btn__buy_now}`}>
+            Buy Now
+          </Link>
+          <Link href="/pricing" className={`${buttons.btn} ${buttons.btn__pricing}`}>
+            See Pricing Plans
+          </Link>
+        </div>
       </div>
     </section>
   )
