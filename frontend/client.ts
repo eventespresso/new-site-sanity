e60d53775d97f9242698f065d0777d0892cce645
tn3rb <brent@eventespresso.com>
2023-03-18 12:05:02 -0700

    add ClientConfig type

--- frontend/client.ts
+++ frontend/client.ts
@@ -1,9 +1,11 @@
-import { createClient } from '@sanity/client';
+import { createClient, type ClientConfig } from '@sanity/client';
 
-export const client = createClient({
+const config: ClientConfig = {
 	projectId: 'rojtizi9', // you can find this in sanity.json
 	dataset: 'production', // or the name you chose in step 1
 	useCdn: true, // `false` if you want to ensure fresh data
 	apiVersion: '2023-01-01', // use current date (YYYY-MM-DD) to target the latest API version
-	token: process.env.SANITY_TOKEN, // Only if you want to update content with the client
-});
+	token: process.env.SANITY_TOKEN, // Only if you wantatest API version
+};
+
+export const client = createClient(config);
