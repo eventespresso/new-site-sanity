a136ffab4bc0fd2335869d71def51f46e069b0b4
tn3rb <brent@eventespresso.com>
2023-03-20 15:35:53 -0700

    create custom ChakraProvider

---
+++ frontend/src/components/layout/Chakra.tsx
@@ -0,0 +1,35 @@
+import { ChakraProvider, cookieStorageManagerSSR, localStorageManager } from '@chakra-ui/react';
+import type { ReactNode } from 'react';
+import type { GetServerSideProps } from 'next'
+
+import theme from '../../styles/theme'
+
+interface ChakraProps {
+	cookies: Partial<{ [key: string]: string; }>;
+	children: ReactNode;
+}
+
+export const Chakra: React.FC<ChakraProps> = ({ cookies, children }) => {
+	// b) Pass `colorModeManager` prop
+	const colorModeManager =
+		typeof cookies === 'string'
+			? cookieStorageManagerSSR(cookies)
+			: localStorageManager
+
+	return (
+		<ChakraProvider colorModeManager={colorModeManager} theme={theme}>
+			{children}
+		</ChakraProvider>
+	)
+}
+
+// also export a reusable function getServerSideProps
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	return {
+		props: {
+			// first time users will not have any cookies and you may not return
+			// undefined here, hence ?? is necessary
+			cookies: req.headers.cookie ?? '',
+		},
+	}
+}
