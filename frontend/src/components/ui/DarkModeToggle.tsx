d73820a880e49253e9abd453d486fefacfa82e1f
tn3rb <brent@eventespresso.com>
2023-03-20 18:30:13 -0700

    tweak DarkModeToggle

--- frontend/src/components/ui/DarkModeToggle.tsx
+++ frontend/src/components/ui/DarkModeToggle.tsx
@@ -1,14 +1,26 @@
-import { IconButton, useColorMode } from '@chakra-ui/react'
+import { Icon, IconButton, useColorMode } from '@chakra-ui/react'
 import { FaMoon, FaSun } from "react-icons/fa";
 
-export const DarkModeToggle: React.FC = () => {
+export const DarkModeToggle: React.FC = (props) => {
   const { colorMode, toggleColorMode } = useColorMode();
+
+  let label = 'Switch to dark mode';
+  let icon = FaMoon;
+
+  if (colorMode === "dark") {
+    label = 'Switch to light mode';
+    icon = FaSun;
+  }
+
   return (
     <IconButton
-      rounded="full"
-      aria-label="change theme"
-      size="xs"
-      onClick={toggleColorMode} icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
+      aria-label={label}
+      colorScheme='yellow'
+      icon={<Icon as={icon} />}
+      onClick={toggleColorMode}
+      size='sm'
+      variant='ghost'
+      {...props}
     />
   );
 }
