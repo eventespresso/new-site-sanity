a42024eb8334e884e9cd3361623abe0a4fcd2ac0
tn3rb <brent@eventespresso.com>
2023-03-22 12:18:03 -0700

    wrap header in container

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,6 +1,5 @@
-import { Button, Flex, HStack, Img, LightMode, Text } from '@chakra-ui/react'
+import { Button, Container, Flex, HStack, Img, LightMode, Text } from '@chakra-ui/react'
 import { DarkModeToggle, InternalLink } from '../ui'
-
 
 import styles from '@/styles/Header.module.scss'
 
@@ -10,46 +9,41 @@ const fontSize = {
 	desktop: '4xl',
 };
 
-const Header: React.FC = () => {
+export const Header: React.FC = () => {
 	return (
-		<Flex as='header' alignItems='center' direction={['column', 'column', 'column', 'row']} gap='2'>
-			<InternalLink href="/" className={styles.espresso_logo__link}>
-				{/* <div className={styles.espresso_logo__img_wrap}> */}
-				<Img src="espresso-cup-logo.png"
-					className={styles.espresso_logo__img}
-					alt="Event Espresso"
-					height='48'
-					width='48'
-					mr={2}
-				/>
-				{/* </div> */}
-				<Text as='span' className={styles.espresso_logo__wordmark} fontSize={fontSize}>event espresso</Text>
-			</InternalLink>
-			<HStack className={styles.primary_nav} direction={['column', 'row']}>
-				<InternalLink href="/pricing" className={styles.nav_link}>
-					pricing
+		<Container maxW='container.wideScreen' >
+			<Flex as='header' alignItems='center' direction={['column', 'column', 'column', 'row']} gap='2'>
+				<InternalLink href="/" className={styles.espresso_logo__link}>
+					<Img src="espresso-cup-logo.png"
+						className={styles.espresso_logo__img}
+						alt="Event Espresso"
+						height='48'
+						width='48'
+						mr={2}
+					/>
+					<Text as='span' className={styles.espresso_logo__wordmark} fontSize={fontSize}>event espresso</Text>
 				</InternalLink>
-				<InternalLink href="/features" className={styles.nav_link}>
-					features
-				</InternalLink>
-				<InternalLink href="/use-cases" className={styles.nav_link}>
-					use-cases
-				</InternalLink>
-				{/* </HStack>
-			<Spacer />
-			<HStack className={styles.secondary_nav} direction={['column', 'row']}> */}
-				<InternalLink href="/account" className={styles.nav_link}>
-					log in
-				</InternalLink>
-				<InternalLink href="/buy-now">
-					<LightMode>
-						<Button as="a" colorScheme='pink' size='sm'>Buy Now</Button>
-					</LightMode>
-				</InternalLink>
-				<DarkModeToggle />
-			</HStack>
-		</Flex>
+				<HStack className={styles.primary_nav} direction={['column', 'row']}>
+					<InternalLink href="/pricing" className={styles.nav_link}>
+						pricing
+					</InternalLink>
+					<InternalLink href="/features" className={styles.nav_link}>
+						features
+					</InternalLink>
+					<InternalLink href="/use-cases" className={styles.nav_link}>
+						use-cases
+					</InternalLink>
+					<InternalLink href="/account" className={styles.nav_link}>
+						log in
+					</InternalLink>
+					<InternalLink href="/buy-now">
+						<LightMode>
+							<Button colorScheme='pink' size='sm'>Buy Now</Button>
+						</LightMode>
+					</InternalLink>
+					<DarkModeToggle />
+				</HStack>
+			</Flex>
+		</Container>
 	);
 }
-
-export default Header;
