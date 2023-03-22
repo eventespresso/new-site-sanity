fc676f9870d0c285b89c8a733b483d6b55a6bad7
tn3rb <brent@eventespresso.com>
2023-03-22 12:17:08 -0700

    add some more types

--- frontend/types.ts
+++ frontend/types.ts
@@ -2,18 +2,37 @@ import type { BlockDefinition, Image, Slug } from '@sanity/types';
 import type { PortableTextBlock } from '@portabletext/types';
 
 export interface Author {
+	_key: string;
+	_type: 'author';
 	bio: Array<BlockDefinition>;
 	image: Image;
 	name: string;
 	slug: Slug;
 }
 
+export interface ContentProps {
+	content: Homepage;
+}
+
+export interface FeaturesSectionProps {
+	keyFeatures: Array<KeyFeature>;
+}
+
+export interface HeroSectionProps {
+	hero_tagline: Array<PortableTextBlock>;
+}
+
 export interface Homepage {
 	slug: Slug;
 	title: string;
 	hero_tagline: Array<PortableTextBlock>;
+	keyFeatures: Array<KeyFeature>;
 }
 
-export interface ContentProps {
-	content: Homepage;
+export interface KeyFeature {
+	_key: string;
+	_type: 'keyFeature';
+	description: Array<PortableTextBlock>;
+	image: Image;
+	title: string;
 }
