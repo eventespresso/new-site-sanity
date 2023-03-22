9851ec85ab48ffbaad16dd809e8f59e7b0be919d
tn3rb <brent@eventespresso.com>
2023-03-22 12:19:13 -0700

    update exports

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,3 +1,5 @@
 export { Chakra, getServerSideProps } from './Chakra';
 export { FeaturesSection } from './FeaturesSection';
+export { Header } from './Header';
+export { HeroSection } from './HeroSection';
 export { Layout } from './Layout';
