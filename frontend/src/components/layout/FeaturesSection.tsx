71def49ca3a0c4512f81766ea09c563285d501c6
tn3rb <brent@eventespresso.com>
2023-03-22 12:19:02 -0700

    fill out FeaturesSection

--- frontend/src/components/layout/FeaturesSection.tsx
+++ frontend/src/components/layout/FeaturesSection.tsx
@@ -1,23 +1,48 @@
 import { PortableText } from '@portabletext/react'
+//CardHeader, CardFooter,
 import {
-	Flex,
-	Button,
+	Box,
+	Card,
+	CardBody,
+	Container,
 	Grid,
-	GridItem,
 	Heading,
-	HStack,
-	Icon,
-	Img,
-	LightMode,
-	Text,
-	VStack,
-	useColorMode
+	Image,
 } from '@chakra-ui/react'
-import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { PortableTextComponents } from '../ui'
+import { imageUrl } from '../../utilities'
+
+import type { FeaturesSectionProps } from '../../../types'
 
 
-export const FeaturesSection: React.FC = () => {
+
+export const FeaturesSection: React.FC<FeaturesSectionProps> = ({ keyFeatures }) => {
 	return (
-		<Flex></Flex>
+		<Box as='section' bg='white' w='100%' p={16}>
+			<Container maxW='container.wideScreen' >
+				<Grid gap={16} templateColumns='repeat(3, 1fr)'>
+					{keyFeatures.map(
+						(keyFeature) => {
+							const source = keyFeature.image;
+							return (
+								<Card key={keyFeature._key} colorScheme='whiteAlpha' direction='column' overflow='hidden' maxW='sm' size='sm' variant='unstyled'>
+									{source && <Image
+										objectFit='cover'
+										// maxW={{ base: '100%', sm: '200px' }}
+										src={imageUrl(source).width(400).url()}
+										alt={keyFeature.image.asset?._key}
+									/>}
+									<CardBody>
+										<Heading size='sm' mt='4'>{keyFeature.title}</Heading>
+										<PortableText value={keyFeature.description} components={PortableTextComponents} />
+									</CardBody>
+
+								</Card>
+							)
+						}
+					)}
+				</Grid>
+			</Container>
+		</Box >
 	);
 }
