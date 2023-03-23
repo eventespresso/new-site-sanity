aa5df63d57e5194fa7c34ebfa4c3ca70183748b7
tn3rb <brent@eventespresso.com>
2023-03-23 15:56:54 -0700

    fix query

--- frontend/src/queries/getPrimaryNav.ts
+++ frontend/src/queries/getPrimaryNav.ts
@@ -2,5 +2,5 @@ import { client } from '../../client';
 import type { GetStaticProps } from 'next'; // GetStaticPaths, GetServerSideProps, GetStaticProps
 
 export const getPrimaryNav: GetStaticProps = async () => {
-	return await client.fetch(`*[_type == "primaryNav"][0]`);
+	return await client.fetch(`*[_type == "primary_nav"][0]`);
 };
